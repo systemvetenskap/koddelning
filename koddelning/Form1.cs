c30e8c568fe90e34b8e25d3a6481ff58f62c54c3
eriobe <erik.oberg@miun.se>
2016-09-06 11:30:16 +0200

    Förändrat texten ytterligare

--- koddelning/Form1.cs
+++ koddelning/Form1.cs
@@ -19,7 +19,7 @@ namespace koddelning
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Det här är en knapp som visar en text");
+            MessageBox.Show("Det här är en knapp som visar en text. Kanske inte världens bästa");
         }
     }
 }
